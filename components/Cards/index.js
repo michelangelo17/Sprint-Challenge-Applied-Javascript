b7f5f3ecb52e1e509accfa5c7ee1fb82c4385586
michelangelo17 <michelangelo.markus@gmail.com>
2019-11-01 09:04:54 -0700

    got article objects for cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -5,7 +5,14 @@
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
-//
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(res => {
+  const sections = Object.values(res.data.articles);
+  sections.forEach(section => section.forEach(article => console.log(article)));
+})
+.catch(err => {
+  console.log(err);
+})
 // <div class="card">
 //   <div class="headline">{Headline of article}</div>
 //   <div class="author">
@@ -17,3 +24,10 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+function cardMaker(obj) {
+    //tools
+    const create = el => document.createElement(el),
+    addClass = (el, aClass) => el.classList.add(aClass),
+    text = (el, textToAdd) => el.textContent = textToAdd,
+    append = (el, parentEl) => parentEl.appendChild(el);
+}
