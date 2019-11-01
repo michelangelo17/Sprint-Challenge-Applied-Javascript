6981857170150956e89980c7861eabb27a5fdd08
michelangelo17 <michelangelo.markus@gmail.com>
2019-11-01 10:18:45 -0700

    completed carousel, made it infinite

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,67 @@
     <div class="right-button"> > </div>
   </div>
 */
+function carouselMaker() {
+  //tools
+  const create = el => document.createElement(el),
+        addClass = (el, aClass) => el.classList.add(aClass),
+        imgAttrs = (el, source, altText) => {
+          el.src = source;
+          el.alt = altText;
+        }
+        appendToCarousel = (el) => carousel.appendChild(el);
+  const carousel = create('div'),
+        leftButton = create('div'),
+        mountains = create('img'),
+        computer = create('img'),
+        trees = create('img'),
+        turntable = create('img'),
+        rightButton = create('div');
+  addClass(carousel, 'carousel');
+  addClass(leftButton, 'left-button');
+  addClass(rightButton, 'right-button');
+  imgAttrs(mountains, '../../assets/carousel/mountains.jpeg', 'moutains');
+  imgAttrs(computer, '../../assets/carousel/computer.jpeg', 'computer')
+  imgAttrs(trees, '../../assets/carousel/trees.jpeg', 'trees')
+  imgAttrs(turntable, '../../assets/carousel/turntable.jpeg', 'turntable');
+  appendToCarousel(leftButton);
+  appendToCarousel(mountains);
+  appendToCarousel(computer);
+  appendToCarousel(trees);
+  appendToCarousel(turntable);
+  appendToCarousel(rightButton);
+  return carousel;
+}
+//tools
+const qs = searchTerm => document.querySelector(searchTerm);
+const qsa = searchTerm => document.querySelectorAll(searchTerm);
+
+const carouselContainer = qs('.carousel-container').appendChild(carouselMaker());
+
+const imgNodeList = qsa('.carousel img');
+
+imgNodeList[0].style.display = 'inline-block';
+
+let imageDisplayed = 0;
+
+qs('.left-button').addEventListener('click', () => {
+  if (imageDisplayed === 0) {
+    imgNodeList[imageDisplayed].style.display = 'none';
+    imageDisplayed = 3;
+  } else {
+    imgNodeList[imageDisplayed].style.display = 'none';
+    imageDisplayed--;
+  }
+  imgNodeList[imageDisplayed].style.display = 'inline-block';
+})
+
+qs('.right-button').addEventListener('click', () => {
+  if (imageDisplayed === 3) {
+    imgNodeList[imageDisplayed].style.display = 'none';
+    imageDisplayed = 0;
+  } else {
+    imgNodeList[imageDisplayed].style.display = 'none';
+    imageDisplayed++;
+  }
+  imgNodeList[imageDisplayed].style.display = 'inline-block';
+})
