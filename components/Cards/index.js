3842f828c342f085d58de072c9039adb67af5038
michelangelo17 <michelangelo.markus@gmail.com>
2019-11-01 09:17:50 -0700

    cards complete, MVP complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -7,8 +7,9 @@
 // Create a function that will programmatically create the following DOM component:
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
 .then(res => {
+  const cardsContainer = document.querySelector('.cards-container');
   const sections = Object.values(res.data.articles);
-  sections.forEach(section => section.forEach(article => console.log(article)));
+  sections.forEach(section => section.forEach(article => cardsContainer.appendChild(cardMaker(article))));
 })
 .catch(err => {
   console.log(err);
@@ -25,9 +26,28 @@ axios.get('https://lambda-times-backend.herokuapp.com/articles')
 //
 // Create a card for each of the articles and add the card to the DOM.
 function cardMaker(obj) {
-    //tools
-    const create = el => document.createElement(el),
-    addClass = (el, aClass) => el.classList.add(aClass),
-    text = (el, textToAdd) => el.textContent = textToAdd,
-    append = (el, parentEl) => parentEl.appendChild(el);
+  //tools
+  const create = el => document.createElement(el),
+        addClass = (el, aClass) => el.classList.add(aClass),
+        text = (el, textToAdd) => el.textContent = textToAdd,
+        append = (el, parentEl) => parentEl.appendChild(el);
+  const card = create('div'),
+        headline = create('div'),
+        author = create('div'),
+        imgContainer = create('div'),
+        authorPhoto = create('img'),
+        authorName = create('span');
+  addClass(card, 'card');
+  addClass(headline, 'headline');
+  addClass(author, 'author');
+  addClass(imgContainer, 'img-container');
+  text(headline, `${obj.headline}`);
+  text(authorName, `By ${obj.authorName}`);
+  authorPhoto.src = `${obj.authorPhoto}`;
+  append(headline, card);
+  append(author, card);
+  append(imgContainer, author);
+  append(authorName, author);
+  append(authorPhoto, imgContainer);
+  return card;
 }
