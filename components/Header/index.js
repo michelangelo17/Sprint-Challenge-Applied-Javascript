25790af7742327dbb86e6a89aea5a9f3a816cffd
michelangelo17 <michelangelo.markus@gmail.com>
2019-11-01 08:40:59 -0700

    completed header. (if time allows, edit less file for top marigin and add weather api for dynamic temperature)

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  //tools
+  const create = el => document.createElement(el),
+        addClass = (el, aClass) => el.classList.add(aClass),
+        text = (el, textToAdd) => el.textContent = textToAdd,
+        append = (el, parentEl) => parentEl.appendChild(el);
+  //create elements
+  const header = create('div'),
+        date = create('span'),
+        siteTitle = create('h1'),
+        temp = create('span');
+  //add classes
+  addClass(header, 'header');
+  addClass(date, 'date');
+  addClass(temp, 'temp');
+  // add text
+  text(date, new Date().toString().split(' ').splice(1,3).join(' '));
+  text(siteTitle, 'Lambda Times');
+  text(temp, '98\u00b0');
+  //append to header
+  append(date, header);
+  append(siteTitle, header);
+  append(temp, header);
+  return header;
+}
+
+document.querySelector('.header-container').appendChild(Header());
